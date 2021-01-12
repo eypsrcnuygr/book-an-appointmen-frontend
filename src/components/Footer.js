7d126aca8e81e67f0b9bc952cd62dd6e3c6252f2
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-01-12 20:01:25 +0300

    CHange the year on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => (
   <footer className="bg-light text-center text-lg-start fixed-bottom myFooter font-weight-bold">
     <div className="text-center p-3">
-      © 2020 Copyright:
-      <a className="text-dark" href="https://www.eyupsercanuygur.com/">Eyüp Sercan UYGUR</a>
+      © 2021 Copyright:
+      <a className="text-dark" target="blank" href="https://www.eyupsercanuygur.com/">Eyüp Sercan UYGUR</a>
     </div>
   </footer>
 );
